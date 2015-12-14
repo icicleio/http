2952e5ffb6cb4dc1fad4b2446db931cf513991a9
Aaron Piotrowski <aaron@trowski.com>
2015-12-13 23:32:11 -0600

    Remove DNS dependency

--- examples/client.php
+++ examples/client.php
@@ -4,19 +4,21 @@
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Icicle\Coroutine;
-use Icicle\Dns;
 use Icicle\Http\Client\Client;
 use Icicle\Http\Driver\Encoder\Http1Encoder;
 use Icicle\Loop;
+use Icicle\Socket;
 
 $coroutine = Coroutine\create(function () {
     $client = new Client();
     $encoder = new Http1Encoder();
 
-    $socket = (yield Dns\connect('www.google.com', 443));
+    // Connect to a google.com IP.
+    // Use Icicle\Dns\connect() in icicleio/dns package to resolve and connect using domain names.
+    $socket = (yield Socket\connect('173.194.46.70', 80));
 
     /** @var \Icicle\Http\Message\Response $response */
-    $response = (yield $client->request($socket, 'GET', 'https://www.google.com/teapot'));
+    $response = (yield $client->request($socket, 'GET', 'http://www.google.com/teapot'));
 
     printf("Headers:\n%s", $encoder->encodeResponse($response));
 
